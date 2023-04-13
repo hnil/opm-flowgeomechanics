4282a193e19cd95a5552bc5e1abae07e13c33b68
hnil <Halvor.M.Nilsen@sintef.no>
2023-04-13 09:15:15 +0200

    partially added bc

--- examples/test_grid.cpp
+++ examples/test_grid.cpp
@@ -10,8 +10,13 @@ int main(int argc, char** argv){
     Dune::FieldVector<double,dim> L = {4.0, 4,0, 4.0};
     std::array<int,dim> s = {1,1,1};
     Grid grid( L , s);
-    auto gv = grid.leafGridView();
+    //auto gv = grid.leafGridView();
+    //auto set = gv.indexSet();
+    //asmhandler_impl.hpp
+    //elasticity_upscale_impl.hpp
     for(const auto& cell:elements(gv)){
+        //auto gm = cell->geometry();
+        //int index2 = set.subIndex(*cell,j,dim);
         for (const auto& is: Dune::intersections(gv,cell)){
             //std::cout << is << std::endl;
         }
