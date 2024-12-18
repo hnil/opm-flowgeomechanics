4ecde3381269b4cf9fc7f0a7057e20785f015f42
Bård Skaflestad <Bard.Skaflestad@sintef.no>
2024-12-18 15:03:39 +0100

    Reduce Object Copying
    In particular, switch to taking parameters as reference to 'const'
    where possible, and return references where meaningful.
    Furthermore, construct objects in place where possible and reduce
    object scopes.

--- opm/geomech/FractureWell.cpp
+++ opm/geomech/FractureWell.cpp
@@ -1,42 +1,58 @@
 #include "config.h"
+
 #include <opm/geomech/FractureWell.hpp>
+
 #include <dune/geometry/referenceelements.hh>
-namespace Opm{
+
+namespace Opm {
     void FractureWell::init(const std::vector<Point3D>& points,
-                   const std::vector<Segment>& segments){
-        Dune::GridFactory<Grid> factory;
-        for(size_t i=0; i < points.size(); ++i){
-            factory.insertVertex(points[i]);
+                            const std::vector<Segment>& segments)
+    {
+        Dune::GridFactory<Grid> factory{};
+
+        for (const auto& vertex : points) {
+            factory.insertVertex(vertex);
         }
-        for (size_t i = 0; i < segments.size(); ++i) {
+
+        for (const auto& segment : segments) {
             std::vector<unsigned int> seg(2,0);
-            seg[0] = segments[i][0];seg[1] = segments[i][1];
+            seg[0] = segment[0];
+            seg[1] = segment[1];
+
             factory.insertElement(Dune::GeometryTypes::line, seg); // std::move(mapping));
         }
+
         grid_ = factory.createGrid();
+
         this->resetWriters();
     }
 
-    void FractureWell::write() const{
-        std::string filename = outputprefix_ + "/" + this->name();
-        vtkwriter_->write(filename.c_str());
+    void FractureWell::write() const
+    {
+        const std::string filename = outputprefix_ + "/" + this->name();
+        vtkwriter_->write(filename);
     }
 
-    void FractureWell::resetWriters(){
+    void FractureWell::resetWriters()
+    {
         // nead to be reseat if grid is changed ??
-        vtkwriter_ = std::make_unique<Dune::VTKWriter<Grid::LeafGridView>>(grid_->leafGridView(), Dune::VTK::nonconforming);
-        std::string outputdir = outputprefix_;
-        std::string simName = casename_ + "_" + this->name();
-        std::string multiFileName =  "";
-        vtkmultiwriter_ = std::make_unique< Opm::VtkMultiWriter<Grid::LeafGridView, VTKFormat > >(/*async*/ false,
-                                                                                              grid_->leafGridView(),
-                                                                                              outputdir,
-                                                                                              simName,
-                                                                                              multiFileName
-        );
+        vtkwriter_ = std::make_unique<Dune::VTKWriter<Grid::LeafGridView>>
+            (grid_->leafGridView(), Dune::VTK::nonconforming);
+
+        const std::string outputdir = outputprefix_;
+        const std::string simName = casename_ + "_" + this->name();
+        const std::string multiFileName =  "";
+
+        vtkmultiwriter_ = std::make_unique<VtkMultiWriter<Grid::LeafGridView, VTKFormat>>
+            (/*async*/ false,
+             grid_->leafGridView(),
+             outputdir,
+             simName,
+             multiFileName);
     }
 
-    void FractureWell::writemulti(double time) const{
+    void FractureWell::writemulti(const double time) const
+    {
         std::vector<double> reservoir_pressure = reservoir_pressure_;
         std::vector<double> reservoir_temperature = reservoir_temperature_;
         //std::vector<double> reservoir_cells = reservoir_cells_;
@@ -54,21 +70,25 @@ namespace Opm{
         vtkmultiwriter_->endWrite();
     }
 
-    FractureWell::FractureWell(std::string outputprefix,
-                               std::string casename, 
-                           std::string name,
-                           const std::vector<Point3D>& points,
-                           const std::vector<Segment>& segments,
-                           const std::vector<int>& res_cells){
-                            outputprefix_ = outputprefix;
-                            casename_ = casename;
+    FractureWell::FractureWell(const std::string& outputprefix,
+                               const std::string& casename,
+                               const std::string& name,
+                               const std::vector<Point3D>& points,
+                               const std::vector<Segment>& segments,
+                               const std::vector<int>& res_cells)
+    {
+        outputprefix_ = outputprefix;
+        casename_ = casename;
         name_ = name;
+
         this->init(points, segments);
+
         vtkwriter_ =
             std::make_unique<Dune::VTKWriter<Grid::LeafGridView>>
             (grid_->leafGridView(), Dune::VTK::nonconforming);
+
         reservoir_cells_ = res_cells;
         reservoir_stress_.resize(res_cells.size());
-        std::fill(reservoir_stress_.begin(),reservoir_stress_.end(),100e5);// random initialization    
+        std::fill(reservoir_stress_.begin(), reservoir_stress_.end(), 100.0e5); // random initialization
     }
 }
